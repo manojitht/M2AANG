9dfa3735873d7b85fbb1b71fda24e76c4bfeda7c
manojitht <manojith12345@outlook.com>
2025-01-04 14:01:36 +0530

    contains duplicate leetcode problem completed

---
+++ 101_leetcode_problems/0001_contains_duplicate.py
@@ -0,0 +1,36 @@
+# Contains Duplicate
+# Solved
+# Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+#
+# Example 1:
+#
+# Input: nums = [1, 2, 3, 3]
+#
+# Output: true
+#
+# Example 2:
+#
+# Input: nums = [1, 2, 3, 4]
+#
+# Output: false
+
+
+# Solution 1:
+
+def has_duplicate_one(nums):
+    temp_arr = set(nums)
+    if len(temp_arr) == len(nums):
+        return False
+    elif len(temp_arr) != len(nums):
+        return True
+
+# Solution 2:
+
+def has_duplicate_two(nums):
+    hashset = set()
+    for i in nums:
+        if i in hashset:
+            return True
+        hashset.add(i)
+    return False
+
