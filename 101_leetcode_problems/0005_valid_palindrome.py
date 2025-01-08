8d7e674f99466d97c6d96b564584e2d6b36be3ce
manojitht <manojith12345@outlook.com>
2025-01-08 23:24:16 +0530

    valid palindrome leetcode problem optimized

--- 101_leetcode_problems/0005_valid_palindrome.py
+++ 101_leetcode_problems/0005_valid_palindrome.py
@@ -19,11 +19,28 @@
 
 # Solution 1
 def is_palindrome(self, s: str) -> bool:
-    # Method 1 using reversing
-    new_str = ""
-    for c in s:
-        if c.isalnum():
-            new_str += c.lower()
-    return new_str == new_str[::-1]
+    # # Method 1 using reversing
+    # new_str = ""
+    # for c in s:
+    #     if c.isalnum():
+    #         new_str += c.lower()
+    # return new_str == new_str[::-1]
 
     # Method 2 using two pointers without built functions for alphanumeric
+    l, r = 0, len(s) - 1
+
+    while l < r:
+        while l < r and not self.alphaNum(s[l]):
+            l += 1
+        while r > l and not self.alphaNum(s[r]):
+            r -= 1
+        if s[l].lower() != s[r].lower():
+            return False
+        l, r = l + 1, r - 1
+    return True
+
+
+def alphaNum(self, c):
+    return (ord('A') <= ord(c) <= ord('Z') or
+            ord('a') <= ord(c) <= ord('z') or
+            ord('0') <= ord(c) <= ord('9'))
