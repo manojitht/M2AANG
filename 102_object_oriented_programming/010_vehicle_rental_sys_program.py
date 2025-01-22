9b0145c219f1440c58285cb4ae4dbad54a754489
manojitht <manojith12345@outlook.com>
2025-01-22 15:25:06 +0530

    oop practice vehicle rental system program completed.

--- 102_object_oriented_programming/010_vehicle_rental_sys_program.py
+++ 102_object_oriented_programming/010_vehicle_rental_sys_program.py
@@ -53,3 +53,167 @@
 # - Allow filtering vehicles by type when displaying.
 
 # Take your time to implement this solution! Let me know if you need hints or guidance. 🚀
+
+
+## My Code:
+class Vehicle:
+    def __init__(self, vehicle_id, vehicle_type, day_rental_price, availability):
+        self.vehicle_id = vehicle_id
+        self.vehicle_type = vehicle_type
+        self.day_rental_price = day_rental_price
+        self.availability = availability
+
+    def discount_eligibility(self, days):
+        if days > 7:
+            discount = (10 / 100) * self.day_rental_price
+            discount_price = self.day_rental_price - discount
+            print(f"Congratulations you are eligible for discount price of Rs.{discount_price}/-")
+        else:
+            discount_price = self.day_rental_price
+        return discount_price
+
+    def rent_vehicle(self, days):
+        day_rent_price = self.discount_eligibility(days)
+        total_cost = days * day_rent_price
+        self.availability = False
+        if not self.availability:
+            status = "Taken"
+        else:
+            status = "Not Taken"
+        print(f"You are rented the vehicle id: {self.vehicle_id}")
+        print(f"-- vehicle type: {self.vehicle_type}")
+        print(f"-- vehicle availability: {status}")
+        print(f"-- per day rental price: {self.day_rental_price}")
+        print(f"-- total rental cost for : {total_cost}")
+
+    def return_vehicle(self):
+        self.availability = True
+        if self.availability:
+            status = "Returned"
+        else:
+            status = "Not Returned"
+        print(f"Returning process of vehicle id: {self.vehicle_id}")
+        print(f"-- vehicle type: {self.vehicle_type}")
+        print(f"-- vehicle availability: {status}")
+
+    def display_vehicle(self):
+        print(f"Vehicle details of vehicle id: {self.vehicle_id}")
+        print(f"-- vehicle type: {self.vehicle_type}")
+        print(f"-- vehicle availability: {self.availability}")
+        print(f"-- per day rental price: {self.day_rental_price}")
+
+
+class Car(Vehicle):
+    def __init__(self, vehicle_id, vehicle_type, day_rental_price, availability, number_of_seats):
+        super().__init__(vehicle_id, vehicle_type, day_rental_price, availability)
+        self.number_of_seats = number_of_seats
+
+    def rent_vehicle(self, days):
+        super().rent_vehicle(days)
+        print(f"-- seats capacity: {self.number_of_seats}")
+
+    def return_vehicle(self):
+        super().return_vehicle()
+        print(f"-- seats capacity: {self.number_of_seats}")
+
+    def display_vehicle(self):
+        super().display_vehicle()
+        print(f"-- seats capacity: {self.number_of_seats}")
+
+
+class Bike(Vehicle):
+    def __init__(self, vehicle_id, vehicle_type, day_rental_price, availability, engine_cc):
+        super().__init__(vehicle_id, vehicle_type, day_rental_price, availability)
+        self.engine_cc = engine_cc
+
+    def rent_vehicle(self, days):
+        super().rent_vehicle(days)
+        print(f"-- engine cc: {self.engine_cc}")
+
+    def return_vehicle(self):
+        super().return_vehicle()
+        print(f"-- engine cc: {self.engine_cc}")
+
+    def display_vehicle(self):
+        super().display_vehicle()
+        print(f"-- engine cc: {self.engine_cc}")
+
+
+class Truck(Vehicle):
+    def __init__(self, vehicle_id, vehicle_type, day_rental_price, availability, load_capacity):
+        super().__init__(vehicle_id, vehicle_type, day_rental_price, availability)
+        self.load_capacity = load_capacity
+
+    def rent_vehicle(self, days):
+        super().rent_vehicle(days)
+        print(f"-- load capacity in tons: {self.load_capacity}")
+
+    def return_vehicle(self):
+        super().return_vehicle()
+        print(f"-- load capacity in tons: {self.load_capacity}")
+
+    def display_vehicle(self):
+        super().display_vehicle()
+        print(f"-- load capacity in tons: {self.load_capacity}")
+
+
+class VehicleRentalSystem:
+    def __init__(self):
+        self.vehicles = []
+
+    def add_vehicle(self, vehicle):
+        self.vehicles.append(vehicle)
+        print(f"Vehicle Id: {vehicle.vehicle_id}, Vehicle Type: {vehicle.vehicle_type}  was added successfully!")
+
+    def rent_vehicle(self, vehicle_id, days):
+        for vehicle in self.vehicles:
+            if vehicle_id == vehicle.vehicle_id and vehicle.availability:
+                return vehicle.rent_vehicle(days)
+            elif not vehicle.availability:
+                return f"Sorry! Vehicle Id: {vehicle_id} is already booked"
+            else:
+                return f"Sorry! No Vehicle Id: {vehicle_id} is available, please check!"
+
+    def return_vehicle(self, vehicle_id):
+        for vehicle in self.vehicles:
+            if vehicle_id == vehicle.vehicle_id and vehicle.availability:
+                return f"This Vehicle Id: {vehicle_id}, already returned"
+            elif vehicle_id == vehicle.vehicle_id and not vehicle.availability:
+                return vehicle.return_vehicle()
+            else:
+                return f"Sorry! No Vehicle Id: {vehicle_id} is exists, please check!"
+
+    def display_all_vehicles(self):
+        count = 0
+        for vehicle in self.vehicles:
+            count += 1
+            print("\n__________________________________________")
+            vehicle.display_vehicle()
+        print("\n__________________________________________")
+        print(f"Total number of vehicles: {count} ")
+
+car = Car(101, "Car", 1000, True, 4)
+bike = Bike(201, "Bike", 300, True, 150)
+truck = Truck(301, "Truck", 2000, True, 10)
+
+rental_company = VehicleRentalSystem()
+rental_company.add_vehicle(car)
+rental_company.add_vehicle(bike)
+rental_company.add_vehicle(truck)
+
+# rental_company.rent_vehicle(101, 3)
+rental_company.rent_vehicle(301, 8)
+rental_company.display_all_vehicles()
+
+
+
+
+
+
+
+
+
+
+
+
+
