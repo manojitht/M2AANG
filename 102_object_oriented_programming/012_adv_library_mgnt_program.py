cd43bef44fecb446e9264a7cc766f6c15210a928
manojitht <manojith12345@outlook.com>
2025-02-04 16:28:57 +0530

    oop practice advanced library management system program add members method completed

--- 102_object_oriented_programming/012_adv_library_mgnt_program.py
+++ 102_object_oriented_programming/012_adv_library_mgnt_program.py
@@ -42,6 +42,16 @@
 # Let me know if you need any clarifications!
 
 ## My Code:
+class Library:
+    def __init__(self):
+        self.books = {}
+        self.librarians = {}
+        self.members = {}
+
+    def search_book(self):
+        pass
+
+
 class Books:
     def __init__(self, book_id, title, author, genre, quantity, publication_year):
         self.book_id = book_id
@@ -54,10 +64,38 @@ class Books:
     def __str__(self):
         return f"Id: {self.book_id}, Title: {self.title}, Author: {self.author}, Genre: {self.genre}, Quantity: {self.quantity}, Year: {self.publication_year}."
 
+
 class Person:
     def __init__(self, name, email):
         self.name = name
         self.email = email
+
+
+class Librarians(Person):
+    generate_book_id = 0
+    generate_member_id = 0
+    def __init__(self, librarian_id, name, email):
+        super().__init__(name, email)
+        self.librarian_id = librarian_id
+
+    @classmethod
+    def add_members(cls, name: str, email: str, address: str):
+        cls.generate_member_id += 1
+        new_member = Members(cls.generate_member_id, name, email, address)
+        library = Library()
+        library.members[cls.generate_member_id] = new_member
+        print(f"New member {name} added to the Library successfully!")
+
+
+    def remove_members(self):
+        pass
+
+    @classmethod
+    def add_book(cls, title, author, genre, quantity, publication_year):
+        pass
+
+    def remove_book(self):
+        pass
 
 
 class Members(Person):
@@ -80,32 +118,6 @@ class Members(Person):
         pass
 
 
-class Librarians(Person):
-    def __init__(self, librarian_id, name, email):
-        super().__init__(name, email)
-        self.librarian_id = librarian_id
-
-    def add_members(self):
-        pass
-
-    def remove_members(self):
-        pass
-
-    def add_book(self):
-        pass
-
-    def remove_book(self):
-        pass
-
-
-class Library:
-    def __init__(self):
-        self.books = {}
-        self.librarians = {}
-        self.members = {}
-
-    def search_book(self):
-        pass
 
 
 
