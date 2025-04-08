83861dcfbca05954b2be5c839559b7397cf5e6e3
manojitht <manojith12345@outlook.com>
2025-04-08 12:51:55 +0530

    feat: add binary search algorithm completed

---
+++ 006_searching_algorithms/002_binary_search_algorithm.py
@@ -0,0 +1,21 @@
+# The binary search algorithm (it's kind a divide and conquer algorithm)
+def binary_search(array, low, high, key):
+    mid = (low + high) // 2
+    if low <= high:
+        if array[mid] == key:
+            print(f"The {key} was found in the array index of {mid}.")
+        elif key > array[mid]:
+            binary_search(array, mid+1, high, key)
+        elif array[mid] > key:
+            binary_search(array, low, mid-1, key)
+    if low > high:
+        print(f"Unsuccessful search and {key} not found in array.")
+
+
+array = [6, 12, 14, 18, 22, 39, 55, 182]
+low = 0
+high = len(array) - 1
+key = 6
+binary_search(array, low, high, key)
+
+# The binary search algorithm big O notation is O (logn)
